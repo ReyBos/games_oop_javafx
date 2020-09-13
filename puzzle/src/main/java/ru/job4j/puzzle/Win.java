1599eaae954cf29bb812adab7835aa7d9bcd9510
reybos <andreybosiy@yandex.ru>
2020-09-13 20:59:41 +0300

    2. Сокобан. Тестовое задание. [#352648] уменьшено количество проверок на выигрыш

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -4,9 +4,11 @@ public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = false;
         for (int i = 0; i < board.length; i++) {
-            if (Win.checkWinColumn(board, i) || Win.checkWinRow(board, i)) {
-                rsl = true;
-                break;
+            if (board[i][i] == 1) {
+                if (Win.checkWinColumn(board, i) || Win.checkWinRow(board, i)) {
+                    rsl = true;
+                    break;
+                }
             }
         }
         return rsl;
